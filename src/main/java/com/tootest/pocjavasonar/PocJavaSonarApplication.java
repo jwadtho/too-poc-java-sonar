805349c94ad7a0aba4d08707a87fa16575b98e63
Jannarong Wadthong <toojannarong@gmail.com>
2021-07-14 04:44:37 +0700

    first commit

---
+++ src/main/java/com/tootest/pocjavasonar/PocJavaSonarApplication.java
@@ -0,0 +1,13 @@
+package com.tootest.pocjavasonar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocJavaSonarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocJavaSonarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tootest/pocjavasonar/PocJavaSonarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tootest.pocjavasonar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocJavaSonarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
